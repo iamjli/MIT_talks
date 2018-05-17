7155f9943bc8035abae8fa24e451e841fbe651d3
Johnny Li <iamjli@mit.edu>
2018-05-17 15:28:30 -0400

    Fix API access

--- src/quick_start.py
+++ src/quick_start.py
@@ -24,4 +24,41 @@ if not labels:
 else:
     print('Labels:')
     for label in labels:
-        print(label['name'])
+        print(label['name'], label['id'])
+
+
+
+from apiclient import errors
+def ListMessagesWithLabels(service, user_id, label_ids=[]):
+    """List all Messages of the user's mailbox with label_ids applied.
+
+    Args:
+        service: Authorized Gmail API service instance.
+        user_id: User's email address. The special value "me"
+        can be used to indicate the authenticated user.
+        label_ids: Only return Messages with these labelIds applied.
+
+    Returns:
+        List of Messages that have all required Labels applied. Note that the
+        returned list contains Message IDs, you must use get with the
+        appropriate id to get the details of a Message.
+    """
+    try:
+        response = service.users().messages().list(userId=user_id, labelIds=label_ids, maxResults=10).execute()
+        messages = []
+        if 'messages' in response:
+            messages.extend(response['messages'])
+
+        while 'nextPageToken' in response:
+            page_token = response['nextPageToken']
+            response = service.users().messages().list(userId=user_id, labelIds=label_ids, pageToken=page_token, maxResults=10).execute()
+            messages.extend(response['messages'])
+
+        return messages
+    except errors.HttpError:
+        print('An error occurred: %s' % error)
+
+
+tmp = ListMessagesWithLabels(service, 'me', 'Label_2744073883326323491')
+print(tmp)
+# ListMessagesWithLabels(service, 'me', 'Talks')
