e4d2d1e3b12256ef4ac41091e02fcf2423f5f71d
Johnny Li <iamjli@mit.edu>
2018-05-16 23:37:57 -0400

    Test Google API script

---
+++ src/quick_start.py
@@ -0,0 +1,27 @@
+"""
+Shows basic usage of the Gmail API.
+
+Lists the user's Gmail labels.
+"""
+from apiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+# Setup the Gmail API
+SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+store = file.Storage('../credentials/credentials.json')
+creds = store.get()
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('../credentials/client_secret.json', SCOPES)
+    creds = tools.run_flow(flow, store)
+service = build('gmail', 'v1', http=creds.authorize(Http()))
+
+# Call the Gmail API
+results = service.users().labels().list(userId='me').execute()
+labels = results.get('labels', [])
+if not labels:
+    print('No labels found.')
+else:
+    print('Labels:')
+    for label in labels:
+        print(label['name'])
