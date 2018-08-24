fefcb202fda1404e0698f6e71a8c344b0edeef65
Johnny Li <iamjli@mit.edu>
2018-08-23 20:03:14 -0400

    Fetch files into python script, session only needs to load once now

---
+++ src/session.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+# Core python modules
+import os
+
+# Peripheral python modules
+import pickle
+
+# Web scraping modules
+import json
+import requests
+from bs4 import BeautifulSoup
+
+
+MIT_LOGIN_PATH = '../credentials/MIT_login.json'
+COOKIES_PATH = '../credentials/cookies.pkl'
+
+
+class Session(): 
+
+	def __init__(self):
+
+		# Import login data
+		self.login_data = json.load(open(MIT_LOGIN_PATH))
+
+		# Try reopenning session with cookies, otherwise open new session
+		try: 
+			self.session = self._load_session()
+		except: 
+			self.session = self._init_login()
+			self._save_session()
+
+		
+	def _init_login(self): 
+		
+		session = requests.session()
+		session.post("http://mailman.mit.edu/mailman/private/mitml/", self.login_data) 
+
+		return session
+
+
+	def _save_session(self): 
+
+		with open(COOKIES_PATH, 'wb') as f: 
+			pickle.dump(requests.utils.dict_from_cookiejar(self.session.cookies), f)
+
+			print("Session cookies saved to: "+COOKIES_PATH)
+
+
+	def _load_session(self): 
+
+		with open(COOKIES_PATH, 'rb') as f: 
+			session = requests.session()
+			session.cookies = requests.utils.cookiejar_from_dict(pickle.load(f))
+
+			print("Session cookies loaded from: "+COOKIES_PATH)
+
+		return session
+
+
+	def get_html(self, url): 
+
+		response = self.session.post(url)
+		return BeautifulSoup(response.text, 'html.parser')
